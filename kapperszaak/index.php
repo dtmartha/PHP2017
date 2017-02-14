cbdea460bd309076e97bf2a8177d11b139011734
dtmartha <dmartha@roc-dev.com>
2017-02-14 12:28:35 +0100

    kapperszaak opdracht afgemaakt.

---
+++ kapperszaak/index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gebruiker
+ * Date: 13-2-2017
+ * Time: 20:35
+ */
+
+$legePlek = "";
+
+$kappersagenda["9.15"] = "Mevr.Pietersen";
+$kappersagenda["9.30"] = "Mevr.Willems";
+$kappersagenda["9.45"] = $legePlek;
+$kappersagenda["10.00"] = "Paul van de Broek";
+$kappersagenda["10.15"] = "Karel de Meeuw";
+$kappersagenda["10.30"] = $legePlek;
+
+foreach ($kappersagenda as $tijden => $klanten) {
+    echo "Tijd: " . $tijden . "," . " Klant: " . $klanten . "<br>";
+
+    for ($i = 0; $i < $tijden; $i++) {
+
+
+    }
+    if ($klanten == $legePlek) {
+        print("<li>" . $tijden . "</li>");
+    }
+
+
+
+}
+
+
+
+
